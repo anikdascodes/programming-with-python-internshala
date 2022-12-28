b14f3b11f3b60d981d3ee97ae43d61502991ca07
Anik Das <110688943+Anik00716@users.noreply.github.com>
2022-12-28 12:15:29 +0530

    Add files via upload

---
+++ module 3 topic 1 ex 1.py
@@ -0,0 +1,7 @@
+Num=int(input("Enter any number :" ))
+if Num>0:
+    print("The number is positive")
+elif Num<0:
+    print("The number is not positive")
+else:
+    print("This is not applicable")
