bd387a1e17c9af4262d15a969d28d85000940101
Anik Das <110688943+Anik00716@users.noreply.github.com>
2024-03-31 17:42:24 +0000

    start

--- module 3 topic 1 ex 1.py
+++
@@ -1,7 +0,0 @@
-Num=int(input("Enter any number :" ))
-if Num>0:
-    print("The number is positive")
-elif Num<0:
-    print("The number is not positive")
-else:
-    print("This is not applicable")
--- module 3 topic 1 ex 2.py
+++
@@ -1,7 +0,0 @@
-Year=int(input("Enter the year: "))
-if (Year %100==0) and (Year%400==0):
-    print("Year" "is leap year")
-elif(Year%4==0) and (Year%100!=0):
-    print("Year is leap year")
-else:
-    print("Year is not leap ")
